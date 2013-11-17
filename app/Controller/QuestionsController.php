fb621a1f61db5e0b94ec8d8cac93c07d9347ee89
mkayokom <mkayokom@gmail.com>
2013-11-17 19:52:03 +0900

    問題詳細の解答一覧

--- app/Controller/QuestionsController.php
+++ app/Controller/QuestionsController.php
@@ -44,7 +44,7 @@ class QuestionsController extends AppController {
  *
  * @var array
  */
-	public $uses = array();
+	public $uses = array('Question','Answer');
 	
 	
 	/*====================
@@ -63,12 +63,14 @@ class QuestionsController extends AppController {
 	 ====================*/
 
 	 	public function detail($questionId = null) { //questionIdが設定されていなかったらnullで初期化する
-		if (!$questionId) { //questionIdが入っていなかったら、エラーを返す
-			throw new NotFoundException(_('Invalid post'));
-		}	
-		$detail = $this->Question->find( 'first', array( 'conditions' => array( 'Question.id' => $questionId ) ) );//questionIdでデータを検索して、$detailに入れる
-		$this->set('detail',$detail);//viewに$detailを渡している
-	
+			if (!$questionId) { //questionIdが入っていなかったら、エラーを返す
+				throw new NotFoundException(_('Invalid post'));
+			}	else  {
+			$question = $this->Question->find( 'first', array( 'conditions' => array( 'Question.id' => $questionId ) ) );//questionIdでデータを検索して、$detailに入れる
+			$this->set('question',$question);//viewに$questionを渡している
+			$answers = $this->Answer->find('all', array('conditions' => array('Answer.question_id' => $questionId )));
+			$this->set('answers',$answers);	
+			}		
 	}//閉じ　function detail
 	
 	
